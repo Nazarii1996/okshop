ce4e999f87159451ce3ba1d7a16e719402a45daf
user <user@example.com>
2018-03-26 14:19:19 +0300

    IGNORE

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://o-k-shop.com.ua/');
+define('HTTP_SERVER', 'http://okshop.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://o-k-shop.com.ua/');
+define('HTTPS_SERVER', 'http://okshop.com/');
 
 // DIR
-define('DIR_APPLICATION', '/home/tcninvbs/public_html/catalog/');
-define('DIR_SYSTEM', '/home/tcninvbs/public_html/system/');
-define('DIR_IMAGE', '/home/tcninvbs/public_html/image/');
-define('DIR_LANGUAGE', '/home/tcninvbs/public_html/catalog/language/');
-define('DIR_TEMPLATE', '/home/tcninvbs/public_html/catalog/view/theme/');
-define('DIR_CONFIG', '/home/tcninvbs/public_html/system/config/');
-define('DIR_CACHE', '/home/tcninvbs/public_html/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home/tcninvbs/public_html/system/storage/download/');
-define('DIR_LOGS', '/home/tcninvbs/public_html/system/storage/logs/');
-define('DIR_MODIFICATION', '/home/tcninvbs/public_html/system/storage/modification/');
-define('DIR_UPLOAD', '/home/tcninvbs/public_html/system/storage/upload/');
+define('DIR_APPLICATION', 'G:/os/OpenServer/domains/okshop.com/catalog/');
+define('DIR_SYSTEM', 'G:/os/OpenServer/domains/okshop.com/system/');
+define('DIR_IMAGE', 'G:/os/OpenServer/domains/okshop.com/image/');
+define('DIR_LANGUAGE', 'G:/os/OpenServer/domains/okshop.com/catalog/language/');
+define('DIR_TEMPLATE', 'G:/os/OpenServer/domains/okshop.com/catalog/view/theme/');
+define('DIR_CONFIG', 'G:/os/OpenServer/domains/okshop.com/system/config/');
+define('DIR_CACHE', 'G:/os/OpenServer/domains/okshop.com/system/storage/cache/');
+define('DIR_DOWNLOAD', 'G:/os/OpenServer/domains/okshop.com/system/storage/download/');
+define('DIR_LOGS', 'G:/os/OpenServer/domains/okshop.com/system/storage/logs/');
+define('DIR_MODIFICATION', 'G:/os/OpenServer/domains/okshop.com/system/storage/modification/');
+define('DIR_UPLOAD', 'G:/os/OpenServer/domains/okshop.coms/ystem/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'tcninvbs_root');
-define('DB_PASSWORD', '0rkl52oE');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'tcninvbs_okshop');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
