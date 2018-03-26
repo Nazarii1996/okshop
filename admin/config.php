2cbf7cfd44d954eb4725e3d8e8aa82f04a4e5e64
Nazarii <lazurchak.nazarii@gmail.com>
2018-03-26 14:45:45 +0300

    test

--- admin/config.php
+++ admin/config.php
@@ -8,24 +8,25 @@ define('HTTPS_SERVER', 'http://o-k-shop.com.ua/admin/');
 define('HTTPS_CATALOG', 'http://o-k-shop.com.ua/');
 
 // DIR
-define('DIR_APPLICATION', 'G:/os/OpenServer/domains/okshop.com/admin/');
-define('DIR_SYSTEM', 'G:/os/OpenServer/domains/okshop.com/system/');
-define('DIR_IMAGE', 'G:/os/OpenServer/domains/okshop.com/image/');
-define('DIR_LANGUAGE', 'G:/os/OpenServer/domains/okshop.com/admin/language/');
-define('DIR_TEMPLATE', 'G:/os/OpenServer/domains/okshop.com/admin/view/template/');
-define('DIR_CONFIG', 'G:/os/OpenServer/domains/okshop.com/system/config/');
-define('DIR_CACHE', 'G:/os/OpenServer/domains/okshop.com/system/storage/cache/');
-define('DIR_DOWNLOAD', 'G:/os/OpenServer/domains/okshop.com/system/storage/download/');
-define('DIR_LOGS', 'G:/os/OpenServer/domains/okshop.com/system/storage/logs/');
-define('DIR_MODIFICATION', 'G:/os/OpenServer/domains/okshop.com/system/storage/modification/');
-define('DIR_UPLOAD', 'G:/os/OpenServer/domains/okshop.com/system/storage/upload/');
-define('DIR_CATALOG', 'G:/os/OpenServer/domains/okshop.com/catalog/');
+define('DIR_APPLICATION', '/home/tcninvbs/public_html/admin/');
+define('DIR_SYSTEM', '/home/tcninvbs/public_html/system/');
+define('DIR_IMAGE', '/home/tcninvbs/public_html/image/');
+define('DIR_LANGUAGE', '/home/tcninvbs/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/tcninvbs/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/tcninvbs/public_html/system/config/');
+define('DIR_CACHE', '/home/tcninvbs/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/tcninvbs/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/tcninvbs/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/tcninvbs/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/tcninvbs/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/tcninvbs/public_html/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'tcninvbs_okshop');
+define('DB_USERNAME', 'tcninvbs_ocart15');
+define('DB_PASSWORD', 'zQP6S[(p38');
+define('DB_DATABASE', 'tcninvbs_ocart15');
 define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
+define('DB_PREFIX', 'ocdh_');
+ 
