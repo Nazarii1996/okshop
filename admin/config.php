197743d535906d4fb35ad40afe4a762a44a5c41c
Nazarii <lazurchak.nazarii@gmail.com>
2018-03-26 14:04:08 +0300

    DataBase and config

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://o-k-shop.com.ua/admin/');
+define('HTTP_CATALOG', 'http://o-k-shop.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://o-k-shop.com.ua/admin/');
+define('HTTPS_CATALOG', 'http://o-k-shop.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/tcninvbs/public_html/admin/');
+define('DIR_SYSTEM', '/home/tcninvbs/public_html/system/');
+define('DIR_IMAGE', '/home/tcninvbs/public_html/image/');
+define('DIR_LANGUAGE', '/home/tcninvbs/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/tcninvbs/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/tcninvbs/public_html/system/config/');
+define('DIR_CACHE', '/home/tcninvbs/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/tcninvbs/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/tcninvbs/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/tcninvbs/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/tcninvbs/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/tcninvbs/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'tcninvbs_root');
+define('DB_PASSWORD', '0rkl52oE');
+define('DB_DATABASE', 'tcninvbs_okshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://o-k-shop.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://o-k-shop.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/tcninvbs/public_html/catalog/');
+define('DIR_SYSTEM', '/home/tcninvbs/public_html/system/');
+define('DIR_IMAGE', '/home/tcninvbs/public_html/image/');
+define('DIR_LANGUAGE', '/home/tcninvbs/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/tcninvbs/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/tcninvbs/public_html/system/config/');
+define('DIR_CACHE', '/home/tcninvbs/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/tcninvbs/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/tcninvbs/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/tcninvbs/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/tcninvbs/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'tcninvbs_root');
+define('DB_PASSWORD', '0rkl52oE');
+define('DB_DATABASE', 'tcninvbs_okshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
